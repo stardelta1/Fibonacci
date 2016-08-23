3f6e7fbc61c69be721aa6aaa0a2178a2703b9520
Sylvester Emeshili <emeshilisylvester@gmail.com>
2016-08-23 10:45:59 -1100

    Fibonacci in Js

---
+++ fibonacci.js
@@ -0,0 +1,12 @@
+
+//[0,1,1,2,3,5,8,13,21,34]
+
+let fibonacci = (limit) => {
+    var fibArr = [0,1];
+    for (var i =1; i < 11; i++) {
+        var nex = fibArr[i] + fibArr[i-1];
+        fibArr.push(nex);
+    }
+    return fibArr;
+}
+fibonacci(15);
